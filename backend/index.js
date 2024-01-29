de2ca26654b6d6f2e6037bb0469dd9d4d9001d0c
Shubham kamboj <114664229+shubhamk98@users.noreply.github.com>
2024-01-29 15:32:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+console.log(process.env.FRONTEND_URL)
 
 app.use("/api/auth", router);
 
